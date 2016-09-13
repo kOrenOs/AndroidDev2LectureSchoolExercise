0cec474f7a1b0750a29eaba78312f87c39c62070
Marek Korenciak <K8048@Labranet.jamk.fi>
2016-09-13 15:27:33 +0300

    init commit

---
+++ AdnroidExercise1SecondLecture/app/src/main/java/com/example/k8048/adnroidexercise1secondlecture/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.k8048.adnroidexercise1secondlecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
